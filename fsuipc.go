e26cd77a3ae3256a330a9a4cc3307b1c9e59364f
Brian Murray <brian@bmurray.ca>
2022-09-28 23:16:25 -0600

    OK?

--- fsuipc.go
+++ fsuipc.go
@@ -1,6 +1,9 @@
 package fsuipc
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"syscall"
 )
 
@@ -14,7 +17,19 @@ func New(name string) (*FSUIPC, error) {
 
 	u := &FSUIPC{}
 	if proc_executeCalclatorCode == nil {
-
+		exePath, err := os.Executable()
+		if err != nil {
+			return nil, err
+		}
+		dllPath := filepath.Join(filepath.Dir(exePath), "FSUIPC_WAPID.dll")
+		if _, err := os.Stat(dllPath); os.IsNotExist(err) {
+			return nil, fmt.Errorf("Cannot open file :%w", err)
+		}
+		mod := syscall.NewLazyDLL(dllPath)
+		if err = mod.Load(); err != nil {
+			return nil, err
+		}
+		proc_executeCalclatorCode = mod.NewProc("executeCalclatorCode")
 	}
 
 	////
