0179c346f0d820948abb2bd47d0393deed1920d1
Brian Murray <brian@bmurray.ca>
2022-09-28 21:49:03 -0600

    first

---
+++ fsuipc.go
@@ -0,0 +1,21 @@
+package fsuipc
+
+import (
+	"syscall"
+)
+
+var proc_executeCalclatorCode *syscall.LazyProc
+
+type FSUIPC struct {
+	x string
+}
+
+func New(name string) (*FSUIPC, error) {
+
+	u := &FSUIPC{}
+	if proc_executeCalclatorCode == nil {
+
+	}
+
+	return u, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bmurray/fsuipc
+
+go 1.19
